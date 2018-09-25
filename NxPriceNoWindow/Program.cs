27d487104b8ea5c1d75b8586cd156033f68f234a
Ruifeng Zhang <ruifeng.zhang@cbb.de>
2018-09-25 11:36:08 +0200

    add no window app

---
+++ NxPriceNoWindow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using nxprice_lib;
+
+namespace NxPriceNoWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NxPriceMain main = new NxPriceMain();
+            main.Start();
+            Console.ReadLine();
+        }
+    }
+}
